94f38af3f3b73c6d4527882e8f844753aeb23ee4
Harlin Seritt <harlin.seritt@gmail.com>
2017-10-03 08:25:55 -0600

    Fixed URL resolver issue when setting slugged_title. Had to reorder the urls

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -5,13 +5,13 @@ from .views import post_detail, posts_filtered
 
 urlpatterns = [
     url(
+        regex=r'^filter/',
+        view=posts_filtered,
+        name='posts_filtered',
+    ),
+    url(
     	regex=r'^(?P<slugged_title>.+)/$',
     	view=post_detail,
     	name='post_detail',
-    ),
-    url(
-    	regex=r'^filter/',
-    	view=posts_filtered,
-    	name='posts_filtered',
     ),
 ]
