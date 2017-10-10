b11c2345ef32cdfb7f183b2c336a7f63d2ac2c17
Harlin Seritt <harlin.seritt@gmail.com>
2017-10-10 05:48:41 -0600

    Add messaging tests

--- blog/messaging/tests.py
+++ blog/messaging/tests.py
@@ -1,6 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import Client, TestCase
+from common.models import PageElement
+from messaging.models import Contact
 
-# Create your tests here.
+
+class MessagingViewsTestCase(TestCase):
+
+    def setUp(self):
+        PageElement.objects.create(name='Tag Line', text='some text')
+        PageElement.objects.create(name='Footer Text', text='some text')
+
+    def test_contact_get(self):
+        client = Client()
+        response = client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Your Name:' in response.content)
+        self.assertTrue('Your Email Address:' in response.content)
+        self.assertTrue('Message:' in response.content)
+
+    def test_contact_post(self):
+        client = Client()
+        response = client.post(
+            '/contact/',
+            {
+                'sender_name': 'Bill',
+                'sender_email': 'bill@localhost',
+                'message': 'this is a message'
+            },
+            follow=True
+        )
+        self.assertTrue(Contact.objects.get(sender_name='Bill'))
+        self.test_contact_get()
